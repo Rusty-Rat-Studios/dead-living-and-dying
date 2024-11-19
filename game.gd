146ea3dacd3ec55ed1856c99fb8f4ba61e894c15
Jacob Meyer <meyer28j@uregina.ca>
2024-11-19 17:27:06 -0600

    Add directional spotlight

--- game.gd
+++ game.gd
@@ -5,7 +5,6 @@ extends Node3D
 # https://www.youtube.com/watch?v=oqFbZoA2lnU
 @onready var state_machine: Node = $StateMachine
 @onready var player: Player = $Player
-@onready var light_omni: OmniLight3D = player.light
 @onready var light_directional: DirectionalLight3D = $DirectionalLight3D
 
 
@@ -34,11 +33,14 @@ func _on_state_entered(state_name: String) -> void:
 	print("State entered: ", state_name)
 	match state_name:
 		"Living":
-			light_omni.visible = true
+			player.light_omni.visible = true
+			player.light_spot.visible = true
 			light_directional.visible = false
 		"Dying":
-			player.light.visible = true
+			player.light_omni.visible = true
+			player.light_spot.visible = true
 			light_directional.visible = false
 		"Dead":
-			player.light.visible = false
+			player.light_omni.visible = false
+			player.light_spot.visible = false
 			light_directional.visible = true
