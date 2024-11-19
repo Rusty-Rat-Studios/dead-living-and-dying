6ca81ec16f63bfb3aef04d6bee775036660bf0f5
Jacob Meyer <meyer28j@uregina.ca>
2024-11-19 16:07:02 -0600

    Prototype for light change on state change

--- state_dead.gd
+++ state_dead.gd
@@ -1,11 +1,18 @@
 extends PlayerState
 
-
 func enter() -> void:
 	super()
 	parent.speed = 10.0
 	# DEBUG: modulate color according to state
 	parent.get_node("RotationOffset/Sprite3D").modulate = Color(0.5, 0.5, 0.5, 0.5)
+	parent.get_node("OmniLight3D").visible = false # disable player light
+	get_parent().get_parent().get_node("DirectionalLight3D").visible = true # enable global light
+	
+
+
+func exit() -> void:
+	parent.get_node("OmniLight3D").visible = true # enable player light
+	get_parent().get_parent().get_node("DirectionalLight3D").visible = false # disable global light
 
 
 func process_input(event: InputEvent) -> State:
