3774b0c059ee1d114f82bdaf05eceec4747721c3
Jacob Meyer <meyer28j@uregina.ca>
2024-11-20 00:05:04 -0600

    Refactor idle movement to Waiting state

--- ghost/ghost.gd
+++ ghost/ghost.gd
@@ -3,69 +3,17 @@ extends CharacterBody3D
 
 @onready var state_machine: Node = $StateMachine
 
+@onready var speed: float = 4.0
 @onready var current_room: Node3D = get_parent()
-var movement_boundaries: Rect2
-
-@onready var speed: float = 4.5 # marginally faster than player in Dying state
-@onready var pause_duration_range: Vector2 = Vector2(1.0, 2.0)
-@onready var target_pos: Vector3 = Vector3.ZERO
-@onready var is_paused: bool = false
-@onready var rng = RandomNumberGenerator.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# dynamically generate bounding box based on floor size of ghost's current room
-	var floor_mesh_instance: MeshInstance3D = current_room.get_node("Floor/MeshInstance3D")
-	var floor_mesh: PlaneMesh = floor_mesh_instance.mesh
-	var floor_width: float = floor_mesh.size.x - 2 # add padding from wall
-	var floor_depth: float = floor_mesh.size.y - 2 # add padding from wall
-	
-	# create movement boundary, Rect2(Vector2 position, Vector2 size)
-	# Rect2 position starts in top-left corner; x-extends right and y-extends down
-	movement_boundaries = Rect2(Vector2(-floor_width / 2, -floor_depth / 2), 
-								Vector2(floor_width, floor_depth))
-	
-	set_random_target()
+	# Initialize state machine
+	# pass reference of the player to the states
+	state_machine.init(self)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float):
-	if is_paused:
-		return
-	
-	if target_pos:
-		move_to_target(delta)
-
-
-func set_random_target() -> void:
-	# generate random movement target within room boundaries
-	var x: float = rng.randf_range(movement_boundaries.position.x,
-					movement_boundaries.position.x + movement_boundaries.size.x)
-	var z: float = rng.randf_range(movement_boundaries.position.y,
-					movement_boundaries.position.y + movement_boundaries.size.y)
-	target_pos = Vector3(x, 1.0, z) # keep ghost above ground
-	is_paused = false
-
-
-func move_to_target(delta: float) -> void:
-	var direction: Vector3 = (target_pos - global_position).normalized()
-	var distance_to_target: float = global_position.distance_to(target_pos)
-	
-	if distance_to_target < speed * delta:
-		# set position to target if close enough
-		global_position = target_pos
-		pause()
-	else:
-		velocity = direction * speed
-		move_and_slide()
-
-
-func pause() -> void:
-	is_paused = true
-	target_pos = global_position
-	
-	var pause_duration: float = rng.randf_range(pause_duration_range.x, pause_duration_range.y)
-	await get_tree().create_timer(pause_duration).timeout # create and wait for timer to elapse
-	
-	set_random_target()
+func _physics_process(delta: float) -> void:
+	# handle basic movement before passing to state-specific actions
+	state_machine.process_physics(delta)
--- ghost/state_waiting.gd
+++ ghost/state_waiting.gd
@@ -1,11 +1,80 @@
 extends GhostState
 
+var movement_boundaries: Rect2 # select random points in room to wander to
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const PAUSE_DURATION_MAX: float = 2.0
+const PAUSE_DURATION_MIN: float = 0.5
+@onready var target_pos: Vector3 = Vector3.ZERO
+@onready var is_paused: bool = false
+@onready var rng = RandomNumberGenerator.new() # generating wait time and target positions
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func enter() -> void:
+	super()
+	parent.speed = 3.0
+	
+	# dynamically generate bounding box based on floor size of ghost's current room
+	var floor_mesh_instance: MeshInstance3D = parent.current_room.get_node("Floor/MeshInstance3D")
+	var floor_mesh: PlaneMesh = floor_mesh_instance.mesh
+	var floor_width: float = floor_mesh.size.x - 2 # add padding from wall to avoid collision
+	var floor_depth: float = floor_mesh.size.y - 2 # add padding from wall to avoid collision
+		
+	# create movement boundary, Rect2(Vector2 position, Vector2 size)
+	# Rect2 position starts in top-left corner; x-extends right and y-extends down
+	movement_boundaries = Rect2(Vector2(-floor_width / 2, -floor_depth / 2), 
+								Vector2(floor_width, floor_depth))
+	
+	# set timer and connect to timeout function
+	$PauseTimer.wait_time = randf_range(PAUSE_DURATION_MIN, PAUSE_DURATION_MAX)
+	$PauseTimer.connect("timeout", Callable(self, "_on_pause_timeout"))
+	
+	set_random_target()
+
+
+func exit() -> void:
+	$PauseTimer.stop()
+	$PauseTimer.disconnect("timeout", Callable(self, "_on_pause_timeout"))
+
+
+func process_physics(delta: float) -> State:
+	if is_paused:
+		return null # stay in waiting state
+	
+	move_to_target(delta)
+	return null # stay in waiting state
+
+
+func set_random_target() -> void:
+	# generate random movement target within room boundaries
+	var x: float = rng.randf_range(movement_boundaries.position.x,
+					movement_boundaries.position.x + movement_boundaries.size.x)
+	var z: float = rng.randf_range(movement_boundaries.position.y,
+					movement_boundaries.position.y + movement_boundaries.size.y)
+	target_pos = Vector3(x, 1.0, z) # keep ghost above ground
+
+
+
+func move_to_target(delta: float) -> void:
+	var direction: Vector3 = (target_pos - parent.global_position).normalized()
+	var distance_to_target: float = parent.global_position.distance_to(target_pos)
+	
+	if distance_to_target < parent.speed * delta:
+		# set target to ghost position if close enough
+		target_pos = parent.global_position
+		pause()
+	else:
+		parent.velocity = direction * parent.speed
+		parent.move_and_slide()
+
+
+func pause() -> void:
+	is_paused = true
+	
+	$PauseTimer.start()
+
+func _on_pause_timeout() -> void:
+	is_paused = false
+	
+	# reset timer
+	$PauseTimer.wait_time = randf_range(PAUSE_DURATION_MIN, PAUSE_DURATION_MAX)
+	set_random_target()
