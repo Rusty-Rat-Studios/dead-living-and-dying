c67a6419e8fb2783340ae1ec8a527839a73b3d60
Cameron Oehler <coehler@hotmail.com>
2024-11-21 12:52:25 -0600

    Revert "Add bad code for testing purposes"
    This reverts commit bac8fc4ab2d73c335cb1e50a441ea64f53247f16.

--- ghost/state_stunned.gd
+++ ghost/state_stunned.gd
@@ -7,5 +7,6 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# gdlint:ignore = unused-argument
 func _process(delta: float) -> void:
 	pass
