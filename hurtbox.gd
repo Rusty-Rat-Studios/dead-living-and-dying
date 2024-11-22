db64a23a0991c65a3b26ae2444770831afbb64d8
Jacob Meyer <meyer28j@uregina.ca>
2024-11-22 12:23:45 -0600

    Update hurtbox code to decouple player reference

--- hurtbox.gd
+++ hurtbox.gd
@@ -1,6 +1,6 @@
 extends Area3D
 
-# responds to player hit timer elapsing
+# flag to indicate whether attack cooldown has expired
 @onready var hit_cooldown_active: bool = false
 # tracks if player is in the area or not
 @onready var player_colliding: bool = false
@@ -9,28 +9,34 @@ extends Area3D
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
-	
-	SignalBus.hit_cooldown_finished.connect(_on_hit_cooldown_finished)
+	$HitCooldown.timeout.connect(_on_hit_cooldown_finished)
 	
 	# just for giggles
 	SignalBus.player_state_changed.connect(_on_state_change)
 
 
 func hurt_player() -> void:
-	if not hit_cooldown_active and player_colliding:
+	if player_colliding and not hit_cooldown_active:
 		SignalBus.emit_signal("player_hurt")
+		# start hit cooldown
 		hit_cooldown_active = true
+		$HitCooldown.start()
 
 
 func _on_body_entered(body: Node3D) -> void:
-	if body == PlayerHandler.get_player():
-		player_colliding = true
-		hurt_player()
+	player_colliding = true
+	hurt_player()
 
 
 func _on_body_exited(body: Node3D) -> void:
-	if body == PlayerHandler.get_player():
-		player_colliding = false
+	player_colliding = false
+
+
+func _on_hit_cooldown_finished() -> void:
+	hit_cooldown_active = false
+	# damage player again if they're still intersecting
+	if player_colliding:
+		hurt_player()
 
 
 func _on_state_change(state_name: String) -> void:
@@ -39,10 +45,3 @@ func _on_state_change(state_name: String) -> void:
 			$Label3D.text = "HURTBOX\n\nCome here to\nget hurt ;_ ;"
 		"Dead":
 			$Label3D.text = "HURTBOX\n\nDon't worry little ghost,\n\nHurtbox can't hurt you."
-
-
-func _on_hit_cooldown_finished() -> void:
-	hit_cooldown_active = false
-	# damage player again if they're still intersecting
-	if player_colliding:
-		hurt_player()
