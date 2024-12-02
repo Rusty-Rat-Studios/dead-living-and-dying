ab02546b2bd4ee18a3dec921ec532a4848aec23e
Jacob Meyer <meyer28j@uregina.ca>
2024-12-01 18:00:34 -0600

    Remove debug statements

--- ui/menu.gd
+++ ui/menu.gd
@@ -7,18 +7,15 @@ func _ready() -> void:
 
 
 func _on_resume_pressed() -> void:
-	print('resume')
 	resume()
 
 
 func _on_quit_pressed() -> void:
-	print('quit')
 	# close the application
 	get_tree().quit()
 
 
 func _input(event: InputEvent) -> void:
-	print("processing input")
 	if event.is_action_pressed("ui_cancel"):
 		if is_visible():
 			resume()
@@ -27,13 +24,11 @@ func _input(event: InputEvent) -> void:
 
 
 func resume() -> void:
-	print('call resume')
 	hide()
 	get_tree().paused = false
 
 
 func pause() -> void:
-	print('call pause')
 	show()
 	get_tree().paused = true
 	$VBoxContainer/Resume.grab_focus()
