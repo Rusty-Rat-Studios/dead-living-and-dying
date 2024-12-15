0a8520659c323337be9a788a513bf22d0174dbb8
Jacob Meyer <meyer28j@uregina.ca>
2024-12-15 16:14:43 -0600

    Implement rotation stopping on joystick release

--- player/player.gd
+++ player/player.gd
@@ -41,7 +41,12 @@ func init(state_machine: Node) -> void:
 
 func _process(delta: float) -> void:
 	if is_rotating:
-		rotate_to_target2(delta)
+		rotate_to_target(delta)
+	
+	var input_dir: Vector2 = Focus.input_get_vector(
+		"joy_right_x_left", "joy_right_x_right", "joy_right_y_up", "joy_right_y_down")
+	if input_dir != Vector2.ZERO:
+		set_light_target_controller()
 
 
 func _physics_process(delta: float) -> void:
@@ -52,11 +57,6 @@ func _input(event: InputEvent) -> void:
 	# check for mouse or joystick-right input
 	if event is InputEventMouseMotion:
 		set_light_target_mouse()
-	elif (event.is_action_pressed("joy_right_x_left") or
-		event.is_action_pressed("joy_right_x_right") or
-		event.is_action_pressed("joy_right_y_up") or
-		event.is_action_pressed("joy_right_y_down")):
-		set_light_target_controller()
 
 
 func handle_movement(delta: float) -> void:
@@ -84,72 +84,33 @@ func handle_movement(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, speed)
 	move_and_slide()
 
-"""
-func set_light_target() -> void:
-	var light_direction: Vector2
-	# check for analog input
-	var input_dir: Vector2 = Focus.input_get_vector(
-		"joy_right_x_left", "joy_right_x_right", "joy_right_y_up", "joy_right_y_down"
-	)
-	if input_dir != Vector2.ZERO:
-		# analog input
-		# scale input to handle tiny analog inputs
-		light_direction = input_dir.normalized() * 100
-		# base target off of player position
-		light_direction += Vector2(position.x, position.z) 
-	else:
-		# check for digital input
-		# prevent mouse from regaining control if it hasn't moved
-		var mouse_pos_2d: Vector2 = get_viewport().get_mouse_position()
-		if mouse_pos_2d != last_mouse_pos: 
-			# player has moved mouse
-			last_mouse_pos = mouse_pos_2d
-			var player_pos_2d: Vector2 = camera.unproject_position(position)
-			light_direction = (mouse_pos_2d - player_pos_2d)
-		else: 
-			# no input, do not move light
-			return
-	
-	light_target = Vector3(light_direction.x, 1, light_direction.y)
-	is_rotating = true
-"""
-
 func set_light_target_mouse() -> void:
-	var light_direction: Vector2
-	# check for digital input
-	# prevent mouse from regaining control if it hasn't moved
 	var mouse_pos_2d: Vector2 = get_viewport().get_mouse_position()
 	var player_pos_2d: Vector2 = camera.unproject_position(position)
-	light_direction = (mouse_pos_2d - player_pos_2d)
+	var light_direction: Vector2 = (mouse_pos_2d - player_pos_2d)
 	
 	light_target = Vector3(light_direction.x, 1, light_direction.y)
 	is_rotating = true
 
 
 func set_light_target_controller() -> void:
-	var a: MeshInstance3D = $MeshInstance3D
-	var light_direction: Vector2
 	var input_dir: Vector2 = Focus.input_get_vector(
 		"joy_right_x_left", "joy_right_x_right", "joy_right_y_up", "joy_right_y_down")
-	# set light_target to edge of field of view
-	#var current_rotation: Vector2 = Vector2.from_angle($LightOffset.rotation.y)
-	var current_rot: float = $LightOffset.rotation.y
-	var current_rotation: Vector2 = Vector2.from_angle(current_rot)
-	a.position = Vector3(current_rotation.x, 1, current_rotation.y).normalized()
-	light_direction = current_rotation.lerp(input_dir, 0.3)
-	# base target off of player position
+	# from_angle() calculates from x-axis, so must be adjusted
+	var current_rotation: Vector2 = Vector2.from_angle(-$LightOffset.rotation.y - PI/2)
+	
+	# set light_target to slightly beside current rotation so that when
+	# the joystick input ceases, the light target is quickly reached.
+	# --- avoids continuing to rotate after input stops
+	var light_direction: Vector2 = current_rotation.lerp(input_dir, 0.3)
+	# base direction vector target off of player position
 	light_direction += Vector2(position.x, position.z)
 	
 	light_target = Vector3(light_direction.x, 1, light_direction.y)
-	print("input dir: ", input_dir)
-	print("current rotation: ", current_rotation)
-	print("light direction: ", light_direction)
-	print("light_target: ", light_target)
-
 	is_rotating = true
 
 
-func rotate_to_target2(delta: float) -> void:
+func rotate_to_target(delta: float) -> void:
 	var light_target_xz: Vector3 = Vector3(light_target.x, 0, light_target.z).normalized()
 	var target_rotation: float = Vector3.FORWARD.signed_angle_to(light_target_xz, Vector3.UP)
 	var current_rotation: float = $LightOffset.rotation.y
@@ -158,14 +119,8 @@ func rotate_to_target2(delta: float) -> void:
 	if abs(angle_diff) > TARGET_THRESHOLD:
 		var max_velocity: float = clamp(abs(angle_diff) * 10.0, 0, MAX_ANGULAR_SPEED)
 		
-		# accelerate up to maximum angular speed
-		#angular_velocity += ANGULAR_ACCELERATION * delta * sign(angle_diff)
-		#angular_velocity = clampf(angular_velocity, -MAX_ANGULAR_SPEED, MAX_ANGULAR_SPEED)
-		
 		# dynamically adjust deceleration as target is approached
 		if abs(angle_diff) < DECELERATION_THRESHOLD:
-			#var weighted_distance: float = 1.0 - abs(angle_diff) / DECELERATION_THRESHOLD
-			#angular_velocity = move_toward(angular_velocity, 0.0, delta * weighted_distance)
 			angular_velocity = move_toward(angular_velocity, sign(angle_diff) * max_velocity, delta * ANGULAR_DECELERATION)
 		else:
 			angular_velocity += ANGULAR_ACCELERATION * delta * sign(angle_diff)
@@ -183,21 +138,6 @@ func rotate_to_target2(delta: float) -> void:
 		is_rotating = false
 	
 	$LightOffset.rotation.y = current_rotation
-
-
-func rotate_to_target1(delta: float) -> void:
-	var light_target_xz: Vector3 = Vector3(light_target.x, 0, light_target.z).normalized()
-	var current_rotation: float = $LightOffset.rotation.y
-	var target_rotation: float = Vector3.FORWARD.signed_angle_to(light_target_xz, Vector3.UP)
-	
-	# interpolate the rotation angle from current to target
-	var new_rotation: float = lerp_angle(current_rotation, target_rotation, MAX_ANGULAR_SPEED * delta)
-	$LightOffset.rotation.y = new_rotation
-	
-	# stop rotating when close enough
-	if abs(new_rotation - target_rotation) < 0.01:
-		$LightOffset.rotation.y = target_rotation
-		is_rotating = false
 
 
 func _on_player_hurt() -> void:
