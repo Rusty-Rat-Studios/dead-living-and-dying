93eebd01db77f45f9a8c0a298f5becfe62423cc9
Jacob Meyer <meyer28j@uregina.ca>
2024-12-30 15:32:42 -0600

    Ignore 'unused signal' warnings in signal bus

--- autoloads/signal_bus.gd
+++ autoloads/signal_bus.gd
@@ -2,23 +2,21 @@ extends Node
 
 # signal for communicating player state to all interested nodes
 # e.g. Game node making universal changes (e.g. lighting) based on player state 
-signal player_state_changed(state_name: String)
+@warning_ignore("unused_signal") signal player_state_changed(state_name: String)
 
 # when the player takes damage. caught by player states to cause state change
-signal player_hurt
-
-signal game_over
+@warning_ignore("unused_signal") signal player_hurt
+@warning_ignore("unused_signal") signal game_over
 
 # signal for communicating player entering and leaving a room
 # passes the room instance which ghosts can match against their
 # current_room variable
-signal player_entered_room(room: Node3D)
-signal player_exited_room(room: Node3D)
+@warning_ignore("unused_signal") signal player_entered_room(room: Node3D)
+@warning_ignore("unused_signal") signal player_exited_room(room: Node3D)
 
 # used when player activates shrine
-signal activated_shrine(shrine: Shrine)
+@warning_ignore("unused_signal") signal activated_shrine(shrine: Shrine)
 # used when player revives at a shrine
-signal consumed_shrine(shrine: Shrine)
-
+@warning_ignore("unused_signal") signal consumed_shrine(shrine: Shrine)
 # emitted when player contacts their corpse in DEAD state
-signal player_revived(corpse_position: Vector3)
+@warning_ignore("unused_signal") signal player_revived(corpse_position: Vector3)
