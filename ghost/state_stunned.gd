f6f8d4fd6e544551f1f3e1519074ff4661ca48d0
Jacob Meyer <meyer28j@uregina.ca>
2024-12-30 14:27:01 -0600

    Remove gdlint:unused-argument lines

--- ghost/state_stunned.gd
+++ ghost/state_stunned.gd
@@ -7,6 +7,5 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-# gdlint:ignore = unused-argument
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
