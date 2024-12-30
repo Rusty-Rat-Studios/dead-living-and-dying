7fe7d7fb73c10845175ddb0049c117f31062d249
Jacob Meyer <meyer28j@uregina.ca>
2024-12-30 02:18:45 -0600

    Add comments for utility functions

--- autoloads/utility.gd
+++ autoloads/utility.gd
@@ -1,10 +1,13 @@
 extends Node
 
-
+# return timeout signal after provided time has elapsed
+# e.g. await Utility.delay(some_time)
 func delay(time: float) -> Signal:
 	return get_tree().create_timer(time).timeout
 
 
+# return the list element that has the closest global_position
+# to the provided argument "global_pos"
 func find_closest(list: Array, global_pos: Vector3) -> Node:
 	var closest: Node = list[0]
 	var distance_sq: float
@@ -16,6 +19,7 @@ func find_closest(list: Array, global_pos: Vector3) -> Node:
 	return closest
 
 
+# call the given function for each item in the list
 func call_for_each(list: Array, function: String) -> void:
 	for item: Node in list:
 		if item.has_method(function):
