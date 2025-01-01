d4df0513dd3c4bebdcd48511f87339da550805e1
Jacob Meyer <meyer28j@uregina.ca>
2024-12-31 21:33:12 -0600

    Refactor shrines to use interactable

--- map/shrine.gd
+++ map/shrine.gd
@@ -4,15 +4,12 @@ extends Area3D
 const TEXT_INACTIVE: String = "SHRINE\r---inactive---\nEnter the shrine\rto activate it"
 const TEXT_ACTIVE: String = "SHRINE\r---ACTIVE---\nYou will revive here if this\ris the closest shrine"
 const TEXT_CONSUMED: String = "SHRINE\r---CONSUMED---\nThis shrine cannot\rbe used anymore"
-#const TEXT_INTERACTABLE: String = "SHRINE\r---inactive---\nPress 'E' to activate"
 const TEXT_INTERACTABLE: String = "[E] Activate"
 
 # tracks if shrine has been reached and activated
 @onready var activated: bool = false
-# tracks whether player is in range to interact with shrine
-@onready var interactable: bool = false
 # tracks if player has revived at this shrine
-# disallows further revivals at this shrine
+# for disallowing further revivals at this shrine
 @onready var consumed: bool = false
 # set only for beginning shrine
 @onready var default: bool = false
@@ -29,36 +26,39 @@ const TEXT_INTERACTABLE: String = "[E] Activate"
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
+	$Interactable.input_detected.connect(_on_interaction)
+	# for edge case of player reviving inside inactive shrine area
+	SignalBus.player_revived.connect(_on_player_revived)
 	
 	# apply material to shrine
 	$MeshInstance3D.set_surface_override_material(0, material)
 	reset()
 
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept") and interactable and not activated:
-		activate()
-
-
 func reset() -> void:
-	$Interactable.inputs = ["interact"]
-	$Interactable.input_detected.connect(activate)
-	activated = false
 	consumed = false
-	material.albedo_color = color_inactive
-	#$Label3D.text = TEXT_INACTIVE
-	$Interactable.display_message(TEXT_INACTIVE)
+	if default:
+		$Label3D.text = TEXT_ACTIVE
+		material.albedo_color = color_active
+	else:
+		$Label3D.text = TEXT_INACTIVE
+		activated = false
+		material.albedo_color = color_inactive
+	
+	$Interactable.inputs = ["interact"]
+	$Interactable.hide_message()
 
 
 func activate() -> void:
 	activated = true
 	# change color and text to show it is activated
 	material.albedo_color = color_active
-	#$Label3D.text = TEXT_ACTIVE
-	$Interactable.display_message(TEXT_ACTIVE)
+	$Label3D.text = TEXT_ACTIVE
+	$Label3D.visible = true
 	SignalBus.activated_shrine.emit(self)
-	# remove input detection for interactable
+	# remove input detection for interactable and hide message
 	$Interactable.inputs.clear()
+	$Interactable.hide_message()
 
 
 func consume() -> void:
@@ -66,31 +66,41 @@ func consume() -> void:
 		consumed = true
 		activated = false
 		material.albedo_color = color_consumed
-		$Interactable.display_message(TEXT_CONSUMED)
-		#$Label3D.text = TEXT_CONSUMED
+		$Label3D.text = TEXT_CONSUMED
 		SignalBus.consumed_shrine.emit(self)
 
 
 func _on_body_entered(_body: Node3D) -> void:
 	# type-checking for player is unneeded as shrine collides on layer PLAYER
 	if not consumed and not activated and PlayerHandler.get_player_state() != "Dead":
-		#activate()
+		$Label3D.visible = false
+		# display interaction text
 		$Interactable.display_message(TEXT_INTERACTABLE)
-		#$Label3D.text = TEXT_INTERACTABLE
-		interactable = true
+		$Interactable.enabled = true
 
 
 func _on_body_exited(_body: Node3D) -> void:
 	# no guard check -> should always be non-interactable when player leaves
 	# regardless of shrine and player states
-	interactable = false
+	$Label3D.visible = true
+	$Interactable.hide_message()
+	$Interactable.enabled = false
 	
 	if activated:
-		$Interactable.display_message(TEXT_ACTIVE)
-		#$Label3D.text = TEXT_ACTIVE
+		$Label3D.text = TEXT_ACTIVE
 	elif consumed:
-		$Interactable.display_message(TEXT_CONSUMED)
-		#$Label3D.text = TEXT_CONSUMED
+		$Label3D.text = TEXT_CONSUMED
 	else:
-		$Interactable.display_message(TEXT_INACTIVE)
-		#$Label3D.text = TEXT_INACTIVE
+		$Label3D.text = TEXT_INACTIVE
+
+
+func _on_interaction(input_name: String) -> void:
+	if input_name == "interact":
+		activate()
+
+
+func _on_player_revived(_corpse_position: Vector3) -> void:
+	# handle edge case of player reviving inside inactive shrine
+	var player: Player = PlayerHandler.get_player()
+	if self.overlaps_body(player):
+		_on_body_entered.call_deferred(player)
--- ui/interactable.gd
+++ ui/interactable.gd
@@ -3,21 +3,26 @@ extends Node3D
 https://forum.godotengine.org/t/display-the-character-of-an-input-on-screen/4101
 '''
 
-# TODO: add code to resume/suspend process when text prompt is visible
-# where _process() continually points the label towards the camera
-
 # used to communicate input back to parent scene
 signal input_detected(event: InputEvent)
 
-# allow multiple input actions for a single node
-# input types should be Input Map strings, e.g. "ui_accept"
+# allow multiple input actions for a single node, parsed in _input()
+# input types should be Input Map strings, e.g. "ui_accept" or "interact"
 @export var inputs: Array = []
 
 @onready var text_box: Label3D = $Label3D
+# flag used to control global signal emission to be caught by parent only
+# otherwise, signals are emitted and caught by all parents of same type
+@onready var enabled: bool = false
+
+
+func _ready() -> void:
+	# match text rotation with camera rotation
+	global_rotation = PlayerHandler.get_player().camera.global_rotation
 
 
 func _input(event: InputEvent) -> void:
-	if not inputs.is_empty():
+	if not inputs.is_empty() and enabled:
 		# check if event matches any input we are listening for
 		for input_name: String in inputs:
 			if event.is_action_pressed(input_name):
