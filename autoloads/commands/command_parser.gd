4b9a3d664619e1e313076f6dc9538350156aee94
Cameron Oehler <coehler@hotmail.com>
2025-01-05 16:24:13 -0600

    Make CommandParser and Commands autoloads & Add DebugLight

--- autoloads/commands/command_parser.gd
+++ autoloads/commands/command_parser.gd
@@ -1,18 +1,18 @@
-class_name CommandParser extends Node
+extends Node
 
 
 # Takes a command string, tokenizes it, processes it, runs it, and returns a response
-static func handle_command(text: String) -> String:
+func handle_command(text: String) -> String:
 	var tokens: PackedStringArray = _tokenize_command(text)
 	return _process_command(tokens)
 
 
-static func _tokenize_command(text: String) -> PackedStringArray:
+func _tokenize_command(text: String) -> PackedStringArray:
 	return text.split(" ", false)
 
 
 # Finds a command definition that matches the tokens supplied and runs the command
-static func _process_command(tokens: PackedStringArray) -> String:
+func _process_command(tokens: PackedStringArray) -> String:
 	var matching_def: String = ""
 	for command_definition: String in Commands.command_map:
 		var def_tokens: PackedStringArray = _tokenize_command(command_definition)
@@ -29,7 +29,7 @@ static func _process_command(tokens: PackedStringArray) -> String:
 
 # Precondition: PackedStringArrays are the same size
 # Returns true if the 2 arrays are the same (where '%s' for a def_token is a wildcard)
-static func _do_tokens_match(tokens: PackedStringArray, def_tokens: PackedStringArray) -> bool:
+func _do_tokens_match(tokens: PackedStringArray, def_tokens: PackedStringArray) -> bool:
 	for i: int in range(tokens.size()):
 		if(def_tokens[i] != "%s"):
 			if(tokens[i] != def_tokens[i]):
@@ -39,7 +39,7 @@ static func _do_tokens_match(tokens: PackedStringArray, def_tokens: PackedString
 
 # Precondition: PackedStringArrays are the same size
 # Returns a PackedStringArray of all the tokens in tokens where there is a '%s' in def_tokens
-static func _get_args(tokens: PackedStringArray, def_tokens: PackedStringArray) -> PackedStringArray:
+func _get_args(tokens: PackedStringArray, def_tokens: PackedStringArray) -> PackedStringArray:
 	var args: Array[String] = []
 	for i: int in range(tokens.size()):
 		if(def_tokens[i] == "%s"):
--- autoloads/commands/commands.gd
+++ autoloads/commands/commands.gd
@@ -1,15 +1,15 @@
-class_name Commands extends Node
+extends Node
 
 
 # Map a command definition to a Callable
-static var command_map: Dictionary = {
+var command_map: Dictionary = {
 	"help": _help,
 	"help %s": _help,
 	"mode %s": _mode
 }
 
 
-static func _help(args: PackedStringArray) -> String:
+func _help(args: PackedStringArray) -> String:
 	if args.size() == 0:
 		return "Available commands: help, mode"
 	if args[0] == "help":
@@ -19,7 +19,11 @@ static func _help(args: PackedStringArray) -> String:
 	return "Usage: help [cmd] - gives usage information on a specific command"
 
 
-static func _mode(args: PackedStringArray) -> String:
+func _mode(args: PackedStringArray) -> String:
 	if not (args[0] == "main" || args[0] == "debug"):
 		return "Usage: mode [main | debug] - changes the game mode"
+	if args[0] == "main":
+		(get_node("/root/Game/DebugLight") as DirectionalLight3D).visible = false
+	elif args[0] == "debug":
+		(get_node("/root/Game/DebugLight") as DirectionalLight3D).visible = true
 	return "Game mode updated"
