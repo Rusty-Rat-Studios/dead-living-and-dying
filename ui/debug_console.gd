ef55c66c7d74a7508eb68d4d96dc4fcc76ece5ba
Cameron Oehler <coehler@hotmail.com>
2025-01-05 14:08:03 -0600

    Catch text enter and print to editor console

--- ui/debug_console.gd
+++ ui/debug_console.gd
@@ -4,6 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Panel/LineEdit.connect("focus_exited", _on_focus_exited)
+	$Panel/LineEdit.connect("text_submitted", _handle_command)
 
 
 func _input(event: InputEvent) -> void:
@@ -20,3 +21,8 @@ func _input(event: InputEvent) -> void:
 
 func _on_focus_exited() -> void:
 	hide()
+
+
+func _handle_command(text: String) -> void:
+	print(text)
+	$Panel/LineEdit.clear()
