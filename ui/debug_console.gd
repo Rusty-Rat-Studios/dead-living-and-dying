5585b98af7d99ecce9324fe86b399714381e9ff1
Cameron Oehler <coehler@hotmail.com>
2025-01-05 13:56:54 -0600

    Open/close DebugConsole with 'console' key. Focus control

--- ui/debug_console.gd
+++ ui/debug_console.gd
@@ -3,9 +3,20 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Panel/LineEdit.connect("focus_exited", _on_focus_exited)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("console"):
+		if not is_visible():
+			$Panel/LineEdit.clear()
+			show()
+		else:
+			hide()
+	if event.is_action_released("console"):
+		if is_visible():
+			$Panel/LineEdit.grab_focus()
+
+
+func _on_focus_exited() -> void:
+	hide()
