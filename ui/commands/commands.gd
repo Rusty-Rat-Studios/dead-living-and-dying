63f40a6795858e777aa06677d73109506485e11f
Cameron Oehler <coehler@hotmail.com>
2025-01-05 15:32:33 -0600

    Add repsonse messages to help and mode commands

--- ui/commands/commands.gd
+++ ui/commands/commands.gd
@@ -1,7 +1,7 @@
 class_name Commands extends Node
 
 
-# map a command definition to a Callable
+# Map a command definition to a Callable
 static var command_map: Dictionary = {
 	"help": help,
 	"help %s": help,
@@ -15,8 +15,18 @@ static func unknown_cmd() -> String:
 
 
 static func help(args: PackedStringArray) -> String:
-	return ""
+	if args.size() == 0:
+		return "Available commands: help, mode"
+	if args[0] == "help":
+		return "Usage: help [cmd] - gives usage information on a specific command"
+	if args[0] == "mode":
+		return "Usage: mode [main | debug] - changes the game mode"
+	return "Usage: help [cmd] - gives usage information on a specific command"
 
 
 static func mode(args: PackedStringArray) -> String:
-	return ""
+	if args.size() != 1:
+		return "Usage: mode [main | debug] - changes the game mode"
+	if not (args[0] == "main" || args[0] == "debug"):
+		return "Usage: mode [main | debug] - changes the game mode"
+	return "Game mode updated"
