d6a82a8af013cfc616472e0724e6756906bf1092
Jacob Meyer <meyer28j@uregina.ca>
2025-01-12 15:07:17 -0600

    Move damage-related code into damage_detector.gd

---
+++ player/damage_detector.gd
@@ -0,0 +1,69 @@
+extends Area3D
+
+# used to set cooldown timer
+const HIT_COOLDOWN: float = 2.0
+# used to flash player sprite while hit cooldown active
+const HIT_FLASH_SPEED: float = 0.3
+const FLASH_OPACITY: float = 0.2
+
+# flag for tracking whether player can take damage or not
+@onready var hit_cooldown_active: bool = false
+# used to toggle opacity while hit cooldown active
+@onready var hit_flash: bool = false
+@onready var sprite: Sprite3D = PlayerHandler.get_player().get_node("RotationOffset/Sprite3D")
+
+func _ready() -> void:
+	$HitCooldown.wait_time = HIT_COOLDOWN
+	$HitFlash.wait_time = HIT_FLASH_SPEED
+	
+	$HitCooldown.timeout.connect(_on_hit_cooldown_timeout)
+	$HitFlash.timeout.connect(_on_hit_flash_timeout)
+	area_entered.connect(_on_enemy_area_entered)
+	print("damage detector ready")
+
+
+func reset() -> void:
+	hit_cooldown_active = false
+	hit_flash = false
+	$HitCooldown.stop()
+	$HitFlash.stop()
+
+
+# utility function for activating i-frames, can be called separately
+# from hit signal
+# e.g. for respawning, to ensure player can't immediately take damage
+# optional "flash" argument to disable the flashing animation
+func activate_hit_cooldown(flash: bool = true) -> void:
+	if not hit_cooldown_active:
+		hit_cooldown_active = true
+		# start hit cooldown
+		$HitCooldown.start()
+		if flash:
+			hit_flash = true
+			$HitFlash.start()
+
+
+func _on_enemy_area_entered(_area: Area3D) -> void:
+	if not hit_cooldown_active:
+		# pass signal for state-specific behavior
+		SignalBus.player_hurt.emit()
+	# do nothing if cooldown active
+
+
+func _on_hit_cooldown_timeout() -> void:
+	# deactivate invincibility frames
+	hit_cooldown_active = false
+	# stop flashing animation
+	$HitFlash.stop()
+	if has_overlapping_areas():
+		# pass signal for state-specific behavior
+		SignalBus.player_hurt.emit()
+
+
+func _on_hit_flash_timeout() -> void:
+	var current_color: Color = sprite.get_modulate()
+	if hit_flash:
+		sprite.modulate = Color(current_color, FLASH_OPACITY)
+	else:
+		sprite.modulate = Color(current_color, 1)
+	hit_flash = not hit_flash
--- player/player.gd
+++ player/player.gd
@@ -1,12 +1,6 @@
 class_name Player
 extends CharacterBody3D
 
-# used to set cooldown timer
-const HIT_COOLDOWN: float = 2.0
-# used to flash player sprite while hit cooldown active
-const HIT_FLASH_SPEED: float = 0.3
-const FLASH_OPACITY: float = 0.2
-
 # player state machine, sibling node under Game node
 var state_machine: Node
 # used to track previously visited, non-consumed shrines (including default)
@@ -15,9 +9,6 @@ var active_shrines: Array
 # used to track player corpse - handled by states
 var corpse: Corpse
 
-@onready var hit_cooldown_active: bool = false
-# used to toggle opacity while hit cooldown active
-@onready var hit_flash: bool = false
 @onready var speed: float = 6.0
 @onready var light_omni: OmniLight3D = $OmniLight3D
 @onready var light_spot: SpotLight3D = $SpotLight3D
@@ -30,13 +21,6 @@ var corpse: Corpse
 
 func _ready() -> void:
 	light_omni.light_color = Color("GOLDENROD")
-	
-	$HitCooldown.wait_time = HIT_COOLDOWN
-	$HitFlash.wait_time = HIT_FLASH_SPEED
-	
-	$HitCooldown.timeout.connect(_on_hit_cooldown_timeout)
-	$HitFlash.timeout.connect(_on_hit_flash_timeout)
-	$DamageDetector.area_entered.connect(_on_enemy_area_entered)
 	
 	SignalBus.activated_shrine.connect(_on_activated_shrine)
 	SignalBus.consumed_shrine.connect(_on_consumed_shrine)
@@ -57,9 +41,7 @@ func init(state_machine: Node, shrine: Shrine, corpse: Corpse) -> void:
 func reset() -> void:
 	# return to starting position and state
 	position = starting_position
-	hit_cooldown_active = false
-	$HitCooldown.stop()
-	$HitFlash.stop()
+	$DamageDetector.reset()
 	
 	# store reference to default shrine before clearing list
 	var default_shrine: Shrine = active_shrines[0]
@@ -102,43 +84,8 @@ func handle_movement(delta: float) -> void:
 	move_and_slide()
 
 
-# utility function for activating i-frames, can be called separately
-# from hit signal
-# e.g. for respawning, to ensure player can't immediately take damage
-# optional "flash" argument to disable the flashing animation
 func take_damage(flash: bool = true) -> void:
-	hit_cooldown_active = true
-	# start hit cooldown
-	$HitCooldown.start()
-	if flash:
-		hit_flash = true
-		$HitFlash.start()
-
-
-func _on_enemy_area_entered(_area: Area3D) -> void:
-	if not hit_cooldown_active:
-		# pass signal for state-specific behavior
-		SignalBus.player_hurt.emit()
-	# do nothing if cooldown active
-
-
-func _on_hit_cooldown_timeout() -> void:
-	# deactivate invincibility frames
-	hit_cooldown_active = false
-	# stop flashing animation
-	$HitFlash.stop()
-	if $DamageDetector.has_overlapping_areas():
-		# pass signal for state-specific behavior
-		SignalBus.player_hurt.emit()
-
-
-func _on_hit_flash_timeout() -> void:
-	var current_color: Color = sprite.get_modulate()
-	if hit_flash:
-		sprite.modulate = Color(current_color, FLASH_OPACITY)
-	else:
-		sprite.modulate = Color(current_color, 1)
-	hit_flash = not hit_flash
+	$DamageDetector.activate_hit_cooldown(flash)
 
 
 func _on_activated_shrine(shrine: Shrine) -> void:
