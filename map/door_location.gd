218a092ae118c55aeb27faa252c4c467d8ae20b4
Cameron Oehler <coehler@hotmail.com>
2025-01-13 18:48:59 -0600

    Add hashing function and equality check to DoorLocation

--- map/door_location.gd
+++ map/door_location.gd
@@ -11,9 +11,22 @@ enum Direction {
 @export var location: Vector2
 @export var direction: Direction
 
+
 func _init(_location:Vector2=Vector2(0,0), _direction:Direction=Direction.NORTH) -> void:
 	location = _location
 	direction = _direction
 
+
 func translate(new_origin: Vector2) -> DoorLocation:
 	return DoorLocation.new(self.location + new_origin, self.direction)
+
+
+func hash() -> PackedByteArray:
+	# Create a string of the form "DoorLocation|(x, y)|d"
+	var data_str: String = "DoorLocation|%s|%s" % [str(location), str(direction)]
+	# Compute the hash
+	return data_str.sha256_buffer()
+
+
+func equals(other: DoorLocation) -> bool:
+	return self.hash() == other.hash()
