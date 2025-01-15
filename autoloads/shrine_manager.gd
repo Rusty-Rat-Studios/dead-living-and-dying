c73ad38cc00c765f3d67533772a807e7b16b9d3f
Cameron Oehler <coehler@hotmail.com>
2025-01-14 20:12:08 -0600

    ShrineManager outline

---
+++ autoloads/shrine_manager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@onready var shrines: Array[Shrine] = []
+
+
+func register_shrine(shrine: Shrine) -> void:
+	pass
+
+
+func get_active_shrines() -> Array[Shrine]:
+	return []
+
+
+func reset() -> void:
+	pass
