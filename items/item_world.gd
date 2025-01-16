cb7abff96584644b20c2292791a7d1c0ddb1eab1
Jacob Meyer <meyer28j@uregina.ca>
2025-01-16 15:57:18 -0600

    Add item base classes

---
+++ items/item_world.gd
@@ -0,0 +1,16 @@
+class_name ItemWorld
+extends Node3D
+
+# to be set by inheritors as a reference to their in-world partner version
+var inventory_resource: Resource
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
