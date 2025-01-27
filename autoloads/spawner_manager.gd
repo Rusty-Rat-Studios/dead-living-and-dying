1996bbd7cefe2b41b197143826cae9960e42b8db
Cameron Oehler <coehler@hotmail.com>
2025-01-27 14:31:06 -0600

    Remove test code

--- autoloads/spawner_manager.gd
+++ autoloads/spawner_manager.gd
@@ -1,14 +1,6 @@
 extends Node
 
 @onready var _spawners: Dictionary = {}
-@onready var entity_table: EntityTable = preload("res://map/test_entity_table.tres")
-
-
-func _ready() -> void:
-	await Utility.delay(1)
-	for spawner: Spawner in get_spawners(Spawner.SpawnerType.ENEMY):
-		var res: Resource = entity_table.get_random_entity()
-		spawner.spawn(res)
 
 
 func register_spawner(spawner: Spawner) -> void:
