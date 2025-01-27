b22846e2dcedad2933865a87be876dd8954a5001
Cameron Oehler <coehler@hotmail.com>
2025-01-27 17:09:49 -0600

    Readd constraints

--- entity/entity_table.gd
+++ entity/entity_table.gd
@@ -12,5 +12,13 @@ func get_random_entity() -> Resource:
 func _get_choices_dictionary_from_entities() -> Dictionary:
 	var choices: Dictionary = {}
 	for entry: EntityTableEntry in entities:
-		choices[entry] = entry.base_chance
+		if entry.max_spawn == -1 or entry.current < entry.max_spawn:
+			choices[entry] = entry.base_chance
 	return choices
+
+
+func are_constraints_met() -> bool:
+	return entities.all(
+		func(entry: EntityTableEntry) -> bool: 
+			return entry.is_within_constraints()
+	)
--- entity/entity_table_entry.gd
+++ entity/entity_table_entry.gd
@@ -4,8 +4,16 @@ extends Resource
 @export var entity: Resource
 @export var base_chance: float
 @export var chance_change: float
+@export var max_spawn: int = -1
+@export var min_spawn: int = -1
+var current: int = 0
 
 
 func get_entity() -> Resource:
 	base_chance += chance_change
+	current += 1
 	return entity
+
+
+func is_within_constraints() -> bool:
+	return current >= min_spawn and (current <= max_spawn or max_spawn == -1)
