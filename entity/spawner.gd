e63afa3590b1977ca4c3ae6fecf68a81b73910d1
Cameron Oehler <coehler@hotmail.com>
2025-01-28 11:37:56 -0600

    Give spawners rotation

--- entity/spawner.gd
+++ entity/spawner.gd
@@ -8,6 +8,7 @@ enum SpawnerType { ENEMY, ITEM, KEY_ITEM, ENTITY_FLOOR, ENTITY_WALL, BOSS }
 
 func _ready() -> void:
 	self.visible = false # Only visible in the editor for debug purposes
+	$RayCast3D.enabled = false
 	SpawnerManager.register_spawner(self)
 
 
@@ -16,6 +17,7 @@ func spawn(entity: Resource) -> void:
 	if entity != null:
 		var node: Node = entity.instantiate()
 		node.position = self.position
+		node.rotation = self.rotation
 		add_sibling(node)
 		print(Time.get_time_string_from_system(), ": Spawner spawned ", node.name)
 	else:
