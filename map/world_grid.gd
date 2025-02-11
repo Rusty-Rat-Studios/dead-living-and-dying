add5cd979273b03d9b1e5727bb1813e5cc279027
Cameron Oehler <coehler@hotmail.com>
2025-02-10 22:56:12 -0600

    Add comments

--- map/world_grid.gd
+++ map/world_grid.gd
@@ -36,6 +36,17 @@ func _clear() -> void:
 	room_map.clear()
 
 
+# Generates a grid of rooms according to information in specified room_table.
+# TODO: Move generation code into its own class
+# Function keeps track of failed placements and will fail generation if number of 
+# consecutive failed placements exceeds GENERATOR_ATTEMPTS.
+# Function returns when all constraints of room_table are met (unless it fails in
+# which case it prints an error and then returns).
+# Steps: pick a random door location on the grid, pick a random room entity, 
+#        check if any of room entity's doors can connect to selected door location,
+#        find a random door that connects and is a valid placement, add locations
+#        room occupies to occupied_grid, add door locations from room to door_grid
+#        (removing and pairs that connect), add room to grid, loop until complete
 func _generate_grid() -> void:
 	var room_table: EntityTable = load("res://entity/entity_tables/test_room_table.tres")
 	var fails: int = 0
@@ -52,13 +63,16 @@ func _generate_grid() -> void:
 		var target_door: DoorLocation = RNG.random_from_list(door_grid)
 		var room_door_dir: DoorLocation.Direction = target_door.invert().direction # Diection of required connecting door
 		
-		print("Selected door %s, needing direction %s" % [target_door, DoorLocation.Direction.keys()[room_door_dir]])
+		print("Selected door %s, needing direction %s" % [target_door.string(), DoorLocation.Direction.keys()[room_door_dir]])
 		
 		var room_information: RoomInformation = room_table.get_random_entity()
 		var valid_room_doors: Array[DoorLocation] = _get_door_locations_facing(room_information, room_door_dir)
 		
+		print("Selected room %s" % [room_information.resource])
+		
 		# Fail if no doors of correct direction
 		if valid_room_doors.size() == 0:
+			print("No doors of that direction exist in room")
 			fails += 1
 			continue
 		
@@ -68,6 +82,7 @@ func _generate_grid() -> void:
 		
 		# Fail if no valid placements
 		if valid_room_placement.has('invalid'):
+			print("No valid placements for room")
 			fails += 1
 			continue
 		
@@ -75,6 +90,7 @@ func _generate_grid() -> void:
 		_add_to_door_grid_removing_matches(valid_room_placement.get('door_grid'))
 		
 		# Add room and reset 'fails'
+		print("Adding room at position %s" % [valid_room_placement.get('room_pos')])
 		_add_room(room_information, valid_room_placement.get('room_pos'))
 		fails = 0
 	
@@ -82,6 +98,10 @@ func _generate_grid() -> void:
 	return
 
 
+# Checks if a random door in valid_room_doors will connect to the location at 
+# room_location without overlapping other rooms. If so, returns the position to
+# place the room at, the occupied_grid of the room, and the door_grid of the room.
+# If no doors are valid, returns { 'invalid': true }
 func _get_valid_room_placement_at_doors(room_information: RoomInformation, room_location: Vector2, 
 	valid_room_doors: Array[DoorLocation]) -> Dictionary:
 	while valid_room_doors.size() > 0:
@@ -103,6 +123,9 @@ func _get_valid_room_placement_at_doors(room_information: RoomInformation, room_
 	return { 'invalid': true }
 
 
+# Takes each DoorLocation in room_door_grid and sees if there is a connecting door
+# in door_grid. If there is, it is removed. If there is not, then the door location
+# is added to the grid.
 func _add_to_door_grid_removing_matches(room_door_grid: Array[DoorLocation]) -> void:
 	for door_location: DoorLocation in room_door_grid:
 		var matched_door: DoorLocation = door_grid.filter(
@@ -116,6 +139,7 @@ func _add_to_door_grid_removing_matches(room_door_grid: Array[DoorLocation]) -> 
 			door_grid.append(door_location)
 
 
+# Returns all the door locations of a room facing a specific direction
 func _get_door_locations_facing(room_information: RoomInformation, dir: DoorLocation.Direction) -> Array[DoorLocation]:
 	return room_information.possible_door_locations.filter(
 		func(value: DoorLocation) -> bool: 
@@ -123,6 +147,7 @@ func _get_door_locations_facing(room_information: RoomInformation, dir: DoorLoca
 	)
 
 
+# Returns the occupied_grid and door_grid of a room
 func _get_room_occupied_and_door_grids(room_info: RoomInformation, grid_location: Vector2) -> Dictionary:
 	var room_occupied: Array[Vector2]
 	room_occupied.assign(room_info.room_shape.map(
@@ -142,6 +167,7 @@ func _get_room_occupied_and_door_grids(room_info: RoomInformation, grid_location
 	}
 
 
+# Returns true if room_occupied_grid overlaps any grid squares in occupied_grid
 func _does_room_overlap_existing(room_occupied_grid: Array[Vector2]) -> bool:
 	return room_occupied_grid.any(func(value: Vector2) -> bool: return value in occupied_grid)
 
