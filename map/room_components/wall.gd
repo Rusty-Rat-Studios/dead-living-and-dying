7dd9b586bb1c140197614dcd4e096808dbe8672e
Cameron Oehler <coehler@hotmail.com>
2025-02-19 17:40:14 -0600

    Move wall.gd and fix gdlint error

--- map/room_components/wall.gd
+++ map/room_components/wall.gd
@@ -2,7 +2,8 @@ class_name Wall
 extends Node3D
 
 const WALL_MATERIAL: StandardMaterial3D = preload("res://map/room_components/wall_material/wall_material.tres")
-const TRANSPARENT_WALL_MATERIAL: ShaderMaterial = preload("res://map/room_components/wall_material/transparent_wall_material.tres")
+const TRANSPARENT_WALL_MATERIAL: ShaderMaterial = \
+	preload("res://map/room_components/wall_material/transparent_wall_material.tres")
 
 const TWEEN_DURATION: float = 1.0
 
