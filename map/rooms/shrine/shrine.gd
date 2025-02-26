4018ea47ca249c5a0aefbc179ee984fa08294552
Jacob Meyer <meyer28j@uregina.ca>
2025-02-25 21:10:13 -0600

    Implement shrine art, collision shape, and scene tree refactor

--- map/rooms/shrine/shrine.gd
+++ map/rooms/shrine/shrine.gd
@@ -1,10 +1,13 @@
 class_name Shrine
-extends Area3D
+extends StaticBody3D
 
 const TEXT_INACTIVE: String = "SHRINE\r---inactive---\nEnter the shrine\rto activate it"
 const TEXT_ACTIVE: String = "SHRINE\r---ACTIVE---\nYou will revive here if this\ris the closest shrine"
 const TEXT_CONSUMED: String = "SHRINE\r---CONSUMED---\nThis shrine cannot\rbe used anymore"
 const TEXT_INTERACTABLE: String = "[E] Activate"
+
+const TEXTURE: Texture2D = preload("res://map/rooms/shrine/shrine.png")
+const TEXTURE_CONSUMED: Texture2D = preload("res://map/rooms/shrine/shrine_consumed.png")
 
 @export var default: bool = false
 
@@ -14,24 +17,16 @@ const TEXT_INTERACTABLE: String = "[E] Activate"
 # for disallowing further revivals at this shrine
 @onready var consumed: bool = false
 # material duplicate for individually modifying material of each shrine
-@onready var material: Material = $MeshInstance3D.get_active_material(0).duplicate()
-# medium green
-@onready var color_inactive: Color = Color(0.1, 0.4, 0.1, 0.25)
-# bright green
-@onready var color_active: Color = Color(0.1, 1, 0.1, 0.25)
-# dark grey
-@onready var color_consumed: Color = Color(0.1, 0.1, 0.1, 0.25)
+@onready var sprite_texture: Texture2D = $Sprite3D.texture
+@onready var detector: Area3D = $PlayerDetector
 
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
+	detector.body_entered.connect(_on_body_entered)
+	detector.body_exited.connect(_on_body_exited)
 	$Interactable.input_detected.connect(_on_interaction)
 	# for edge case of player reviving inside inactive shrine area
 	SignalBus.player_revived.connect(_on_player_revived)
-	
-	# apply material to shrine
-	$MeshInstance3D.set_surface_override_material(0, material)
 	
 	# register the shrine with ShrineManager
 	ShrineManager.register_shrine(self)
@@ -42,13 +37,11 @@ func _ready() -> void:
 func reset() -> void:
 	consumed = false
 	if default:
-		$Label3D.text = TEXT_ACTIVE
 		activated = true
-		material.albedo_color = color_active
+		#material.albedo_color = color_active
 	else:
-		$Label3D.text = TEXT_INACTIVE
 		activated = false
-		material.albedo_color = color_inactive
+		#material.albedo_color = color_inactive
 	
 	$Interactable.inputs = ["interact"]
 	$Interactable.hide_message()
@@ -57,9 +50,7 @@ func reset() -> void:
 func activate() -> void:
 	activated = true
 	# change color and text to show it is activated
-	material.albedo_color = color_active
-	$Label3D.text = TEXT_ACTIVE
-	$Label3D.visible = true
+	#material.albedo_color = color_active
 	# remove input detection for interactable and hide message
 	$Interactable.inputs.clear()
 	$Interactable.hide_message()
@@ -69,16 +60,18 @@ func consume() -> void:
 	if not default:
 		consumed = true
 		activated = false
-		material.albedo_color = color_consumed
-		$Label3D.text = TEXT_CONSUMED
+		#material.albedo_color = color_consumed
 
 
 func _on_body_entered(_body: Node3D) -> void:
+	print("entered")
+	print("consumed: ", consumed)
+	print("activated: ", activated)
 	# type-checking for player is unneeded as shrine collides on layer PLAYER
 	if (not consumed 
 		and not activated 
 		and PlayerHandler.get_player_state() != PlayerStateMachine.States.DEAD):
-		$Label3D.visible = false
+		print("player can interact!")
 		# display interaction text
 		$Interactable.display_message(TEXT_INTERACTABLE)
 		$Interactable.enabled = true
@@ -87,16 +80,8 @@ func _on_body_entered(_body: Node3D) -> void:
 func _on_body_exited(_body: Node3D) -> void:
 	# no guard check -> should always be non-interactable when player leaves
 	# regardless of shrine and player states
-	$Label3D.visible = true
 	$Interactable.hide_message()
 	$Interactable.enabled = false
-	
-	if activated:
-		$Label3D.text = TEXT_ACTIVE
-	elif consumed:
-		$Label3D.text = TEXT_CONSUMED
-	else:
-		$Label3D.text = TEXT_INACTIVE
 
 
 func _on_interaction(input_name: String) -> void:
@@ -107,5 +92,5 @@ func _on_interaction(input_name: String) -> void:
 func _on_player_revived(_corpse_position: Vector3) -> void:
 	# handle edge case of player reviving inside inactive shrine
 	var player: Player = PlayerHandler.get_player()
-	if self.overlaps_body(player):
+	if detector.overlaps_body(player):
 		_on_body_entered.call_deferred(player)
