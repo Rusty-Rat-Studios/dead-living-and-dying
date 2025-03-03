2f61aa0d72d04936a0b2bdb26e9aea8a4058391a
Quinn Maloney <qkmaloney@gmail.com>
2025-03-03 12:28:42 -0600

    fixed error message
    fixed error message to read correctly.

--- items/active_item_inventory.gd
+++ items/active_item_inventory.gd
@@ -3,4 +3,4 @@ extends ItemInventory
 
 
 func use() -> void:
-	push_error("AbstractMethodError: DefenseItemInventory use() function called from base DefenseItemInventory class")
+	push_error("AbstractMethodError: ActiveItemInventory use() function called from base ActiveItemInventory class")
