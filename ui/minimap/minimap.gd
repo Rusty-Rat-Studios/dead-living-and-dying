2aec717a1b2ece7fb71e38ead8f0ef31b11178c4
Cameron Oehler <coehler@hotmail.com>
2025-03-12 18:22:34 -0600

    Fix gdlint errors

--- ui/minimap/minimap.gd
+++ ui/minimap/minimap.gd
@@ -5,5 +5,5 @@ extends Control
 @onready var player: Player = PlayerHandler.get_player()
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	camera.global_position = Vector3(player.global_position.x, camera.global_position.y, player.global_position.z)
