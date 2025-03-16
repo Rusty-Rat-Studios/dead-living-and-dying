f3b45a1d15c7f29b2e22d50f0bdfa774b94c34d6
Cameron Oehler <coehler@hotmail.com>
2025-03-15 19:29:13 -0600

    Move all game stuff to src directory

---
+++ src/map/room_components/room.gd
@@ -0,0 +1,107 @@
+class_name Room
+extends Node3D
+
+# The base class for all rooms. Stores information about the room, 
+# the position on the world grid, and the possessables in the room.
+# On init the room moves to its specified grid location and runs init_doors()
+
+signal player_discovered_room
+
+const ICON_MINIMAP: Resource = preload("res://src/ui/minimap/icon_minimap.tscn")
+
+@export var spawn_room: bool = false
+@export var room_information: RoomInformation
+
+var grid_location: Vector2
+var possessables_available: Array
+var doors: HashMap = HashMap.new()
+var room_discovered: bool = false
+
+@onready var player_in_room: bool = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Floor/PlayerDetector.body_entered.connect(_on_player_entered_room)
+	$Floor/PlayerDetector.body_exited.connect(_on_player_exited_room)
+	player_discovered_room.connect(_on_player_discovered_room)
+	visible = false
+
+
+func init() -> void:
+	var grid_scale: float = WorldGrid.GRID_SCALE
+	var room_location: Vector3 = Vector3(grid_location.x * grid_scale, 0, 
+		grid_location.y * grid_scale)
+	global_position = room_location
+	init_doors()
+	print(Time.get_time_string_from_system(), ": Room ", grid_location , " initialized")
+
+
+func reset() -> void:
+	room_discovered = false
+	if spawn_room:
+		player_discovered_room.emit()
+
+
+func add_possessable(possessable: Possessable) -> void:
+	possessables_available.append(possessable)
+
+
+func remove_possessable(possessable: Possessable) -> void:
+	var index: int = possessables_available.find(possessable)
+	if index != -1:
+		# possessable was found in array
+		possessables_available.remove_at(index)
+	else:
+		print(Time.get_time_string_from_system(), 
+		": WARNING: Attempted to remove a possessable that is not in ", name, "'s array")
+
+
+func register_door(door: Door) -> void:
+	doors.add(door.door_location, door)
+
+
+func deregister_door(door: Door) -> void:
+	var success: bool = doors.remove(door.door_location)
+	if not success:
+		push_error("ERROR: Room.deregister_door failed")
+
+
+func get_door_at_location(grid_door_location: DoorLocation) -> Door:
+	var door_location: DoorLocation = grid_door_location.translate(-grid_location)
+	return doors.retrieve(door_location)
+
+
+func init_doors() -> void:
+	for door: Door in doors.values():
+		door.init(grid_location)
+
+
+func _on_player_entered_room(body: Node3D) -> void:
+	if body == PlayerHandler.get_player():
+		player_in_room = true
+		visible = true
+		SignalBus.player_entered_room.emit(self)
+		if not room_discovered:
+			player_discovered_room.emit()
+
+
+func _on_player_exited_room(body: Node3D) -> void:
+	if body == PlayerHandler.get_player():
+		player_in_room = false
+		visible = false
+		SignalBus.player_exited_room.emit(self)
+
+
+func _on_player_discovered_room() -> void:
+	room_discovered = true
+	
+	var minimap_component: Node3D = room_information.minimap_component.instantiate()
+	$/root/Game/MinimapObjects.add_child(minimap_component)
+	minimap_component.global_position = global_position
+	
+	if room_information.room_icon != null:
+		var room_icon: Node3D = ICON_MINIMAP.instantiate()
+		$/root/Game/MinimapObjects.add_child(room_icon)
+		room_icon.global_position = global_position
+		room_icon.get_child(0).texture = room_information.room_icon
---
+++ src/map/world_grid.gd
@@ -0,0 +1,131 @@
+class_name WorldGrid
+extends Node3D
+
+# This class is the authority on everything room related. It handles instantiation, 
+# initialization, and storing the location of the rooms. Other classes call this
+# class to ask about the state of the WorldGrid.
+
+const GRID_SCALE: float = 16 # Size of each grid square in editor units
+
+@export var generator_settings: GeneratorSettings = null
+
+var room_map: HashMap = HashMap.new()
+
+
+func _ready() -> void:
+	setup_grid()
+
+
+func setup_grid() -> void:
+	var occupied_and_door_grids: Dictionary[String, Array] = _load_grid_with_current_scene()
+	
+	if generator_settings != null and generator_settings.room_table != null:
+		var generator: WorldGenerator = WorldGenerator.new(
+			generator_settings,
+			occupied_and_door_grids.get('occupied_grid'),
+			occupied_and_door_grids.get('door_grid')
+		)
+		generator.generate_grid(self)
+	_init_all_rooms()
+	_spawn_entities()
+
+
+func add_room(room: Room, grid_location: Vector2, add_to_tree: bool = true) -> void:
+	print(Time.get_time_string_from_system(), 
+		": Added room at location ", grid_location)
+	room.grid_location = grid_location
+	# Adds each grid square that the room takes up to the HashMap & occupied_grid
+	for room_portion: Vector2 in room.room_information.room_shape:
+		var translated_room_portion: Vector2 = room_portion + grid_location
+		room_map.add_with_hash(_hash_vector2(translated_room_portion), room)
+	if add_to_tree:
+		add_child(room)
+
+
+# If grid_location exists in the HashMap return room, otherwise returns null
+func get_room_at_location(grid_location: Vector2) -> Room:
+	return room_map.retrieve_with_hash(_hash_vector2(grid_location))
+
+
+func clear() -> void:
+	for node: Node in get_children():
+		if node is Room:
+			node.queue_free()
+	room_map.clear()
+
+
+func _load_grid_with_current_scene() -> Dictionary[String, Array]:
+	var occupied_grid: Array[Vector2] = []
+	var door_grid: Array[DoorLocation] = []
+	for room: Room in find_children("*", "Room"):
+		var grid_location: Vector2 = (
+			Vector2(room.global_position.x, room.global_position.z)/GRID_SCALE
+			).round()
+		add_room(room, grid_location, false)
+		var room_occupied_and_door_grids: Dictionary[String, Array] = get_room_occupied_and_door_grids(
+			room.room_information, grid_location)
+		occupied_grid.append_array(room_occupied_and_door_grids.get("occupied_grid"))
+		add_to_door_grid_removing_matches(door_grid, room_occupied_and_door_grids.get("door_grid"))
+	return {
+		"occupied_grid": occupied_grid,
+		"door_grid": door_grid
+	}
+
+
+func _init_all_rooms() -> void:
+	for node: Node in self.get_children():
+		if node is Room:
+			node.init()
+
+
+func _spawn_entities() -> void:
+	if generator_settings != null:
+		SpawnerManager.spawn(Spawner.SpawnerType.ENEMY, generator_settings.enemy_entity_table)
+		SpawnerManager.spawn(Spawner.SpawnerType.ITEM, generator_settings.enemy_entity_table)
+		SpawnerManager.spawn(Spawner.SpawnerType.KEY_ITEM, generator_settings.enemy_entity_table)
+		SpawnerManager.spawn(Spawner.SpawnerType.ENTITY_FLOOR, generator_settings.enemy_entity_table)
+		SpawnerManager.spawn(Spawner.SpawnerType.ENTITY_WALL, generator_settings.enemy_entity_table)
+		SpawnerManager.spawn(Spawner.SpawnerType.BOSS, generator_settings.enemy_entity_table)
+
+
+# Returns the occupied_grid and door_grid of a room
+static func get_room_occupied_and_door_grids(room_info: RoomInformation, 
+	grid_location: Vector2) -> Dictionary[String, Array]:
+	var room_occupied: Array[Vector2]
+	room_occupied.assign(room_info.room_shape.map(
+		func(value: Vector2) -> Vector2:
+			return value + grid_location
+	))
+	
+	var room_doors: Array[DoorLocation]
+	room_doors.assign(room_info.possible_door_locations.map(
+		func(value: DoorLocation) -> DoorLocation:
+			return value.translate(grid_location)
+	))
+	
+	return {
+		'occupied_grid': room_occupied,
+		'door_grid': room_doors
+	}
+
+
+# Takes each DoorLocation in room_door_grid and sees if there is a connecting door
+# in door_grid. If there is, it is removed. If there is not, then the door location
+# is added to the grid. The resulting grid is returned.
+static func add_to_door_grid_removing_matches(door_grid: Array[DoorLocation], 
+	room_door_grid: Array[DoorLocation]) -> void:
+	for door_location: DoorLocation in room_door_grid:
+		var matches: Array[DoorLocation] = door_grid.filter(
+			func(value: DoorLocation) -> bool: 
+				return value.equals(door_location.invert())
+		)
+		
+		if matches.size() > 0:
+			door_grid.erase(matches.front())
+		else:
+			door_grid.append(door_location)
+
+
+func _hash_vector2(vector: Vector2) -> PackedByteArray:
+	var hash_string: String = "Vector2|%s" % [str(vector)]
+	return hash_string.sha256_buffer()
