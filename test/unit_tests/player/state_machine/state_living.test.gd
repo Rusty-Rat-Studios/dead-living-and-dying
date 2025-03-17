a5ad9ecef4110c088c9cb9f2fd0f50d90db980d2
Cameron Oehler <coehler@hotmail.com>
2025-03-16 20:59:33 -0600

    Add test 1.1.2

--- test/unit_tests/player/state_machine/state_living.test.gd
+++ test/unit_tests/player/state_machine/state_living.test.gd
@@ -1,5 +1,6 @@
 extends GutTest
 
+# Test 1.1.1
 class TestPlayerEnterLivingState:
 	extends GutTest
 	
@@ -20,14 +21,13 @@ class TestPlayerEnterLivingState:
 		player = null
 	
 	
-	func test_player_enters_living_state() -> void:
-		player._state_machine.change_state(PlayerStateMachine.States.DEAD)
+	func test_player_in_living_state() -> void:
 		player._state_machine.change_state(PlayerStateMachine.States.LIVING)
+		
 		assert_eq(player._state_machine.current_state, PlayerStateMachine.States.LIVING)
 	
 	
 	func test_living_state_variables_set_correctly() -> void:
-		player._state_machine.change_state(PlayerStateMachine.States.DEAD)
 		player._state_machine.change_state(PlayerStateMachine.States.LIVING)
 		
 		assert_called(player, 'inventory_update')
@@ -37,7 +37,33 @@ class TestPlayerEnterLivingState:
 	func test_state_changed_signal_emitted() -> void:
 		watch_signals(SignalBus)
 		
-		player._state_machine.change_state(PlayerStateMachine.States.DEAD)
 		player._state_machine.change_state(PlayerStateMachine.States.LIVING)
 		
 		assert_signal_emitted_with_parameters(SignalBus, 'player_state_changed', [PlayerStateMachine.States.LIVING])
+
+
+# Test 1.1.2
+class TestPlayerExitLivingState:
+	extends GutTest
+	
+	var basic_test_scene: Node3D
+	var player: Player
+
+	
+	func before_each() -> void:
+		basic_test_scene = preload("res://test/unit_tests/basic_test_scene.tscn").instantiate()
+		player = basic_test_scene.get_node("Player")
+		player.set_script(partial_double(preload("res://src/player/player.gd")))
+		get_tree().root.add_child(basic_test_scene)
+	
+	
+	func after_each() -> void:
+		basic_test_scene.free()
+		basic_test_scene = null
+		player = null
+	
+	
+	func test_player_not_in_living_state() -> void:
+		player._state_machine.change_state(PlayerStateMachine.States.DYING)
+		
+		assert_ne(player._state_machine.current_state, PlayerStateMachine.States.LIVING)
