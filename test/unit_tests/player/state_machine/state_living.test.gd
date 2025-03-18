c99c8dd2bb9870857de79638ad261f721a867713
Cameron Oehler <coehler@hotmail.com>
2025-03-17 21:27:31 -0600

    Tests 1.1.1 & 1.1.2

--- test/unit_tests/player/state_machine/state_living.test.gd
+++ test/unit_tests/player/state_machine/state_living.test.gd
@@ -4,15 +4,46 @@ extends GutTest
 class TestPlayerEnterLivingState:
 	extends GutTest
 	
-	var _state_machine: PlayerStateMachine
-	var _player: CharacterBody3D
+	var _state_living: PlayerState
 	
 	func before_each() -> void:
-		_state_machine = PlayerStateMachine.new()
-		_player = double(CharacterBody3D).new()
-		_state_machine.init(_player)
+		_state_living = load("res://src/player/state_machine/state_living.gd").new()
+		_state_living._parent = double(Player).new()
+		stub(_state_living._parent, "inventory_update").to_do_nothing()
+		_state_living._state_machine = double(PlayerStateMachine).new()
+		_state_living._state_machine.current_state = PlayerStateMachine.States.LIVING
 	
-	func test_player_in_living_state() -> void:
-		_state_machine.change_state(PlayerStateMachine.States.LIVING)
+	
+	func test_living_state_variables_set_correctly() -> void:
+		_state_living.enter()
 		
-		assert_eq(_state_machine.current_state, PlayerStateMachine.States.LIVING)
+		assert_called(_state_living._parent, "inventory_update")
+		assert_connected(SignalBus, _state_living, 'player_hurt')
+	
+	
+	func test_state_changed_signal_emitted() -> void:
+		watch_signals(SignalBus)
+		_state_living.enter()
+		
+		assert_signal_emitted_with_parameters(SignalBus, 'player_state_changed', [PlayerStateMachine.States.LIVING])
+
+
+# Test 1.1.2
+class TestPlayerExitLivingState:
+	extends GutTest
+	
+	var _state_living: PlayerState
+	
+	func before_each() -> void:
+		_state_living = load("res://src/player/state_machine/state_living.gd").new()
+		_state_living._parent = double(Player).new()
+		stub(_state_living._parent, "inventory_update").to_do_nothing()
+		_state_living._state_machine = double(PlayerStateMachine).new()
+		_state_living._state_machine.current_state = PlayerStateMachine.States.LIVING
+		_state_living.enter()
+	
+	
+	func test_living_state_variables_unset_correctly() -> void:
+		_state_living.exit()
+		
+		assert_not_connected(SignalBus, _state_living, 'player_hurt')
