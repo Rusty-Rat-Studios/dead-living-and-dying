31a9ac989761b9e44f8b444994c52c6c38befeff
Cameron Oehler <coehler@hotmail.com>
2025-03-22 12:17:16 -0600

    Add wall export for wall_mesh

--- src/map/room_components/wall.gd
+++ src/map/room_components/wall.gd
@@ -6,6 +6,11 @@ signal values_changed
 
 const TWEEN_DURATION: float = 1.0
 
+@export var wall_mesh: MeshInstance3D:
+	set(value):
+		wall_mesh = value
+		values_changed.emit()
+
 @export var preview_transparent_material: bool:
 	set(value):
 		preview_transparent_material = value
@@ -34,9 +39,9 @@ func _ready() -> void:
 	if not Engine.is_editor_hint():
 		# Reset mesh material if previously set via tool script
 		if wall_material != null:
-			$WallMesh.set_surface_override_material(0, wall_material)
+			wall_mesh.set_surface_override_material(0, wall_material)
 		else:
-			$WallMesh.set_surface_override_material(0, null)
+			wall_mesh.set_surface_override_material(0, null)
 		
 		if fmod(global_rotation_degrees.y, 180) == 0:
 			is_horizontal_wall = true
@@ -45,7 +50,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if not Engine.is_editor_hint(): # only run in engine
 		if is_horizontal_wall:
-			$WallMesh.set_instance_shader_parameter("intensity", intensity)
+			wall_mesh.set_instance_shader_parameter("intensity", intensity)
 			if (PlayerHandler.get_player().global_position.z < global_position.z) and not is_transparent:
 				_apply_material_transparent()
 			elif (PlayerHandler.get_player().global_position.z > global_position.z) and is_transparent:
@@ -55,7 +60,7 @@ func _process(_delta: float) -> void:
 # Changes material to transparent then tweens transparency intensity to 1
 func _apply_material_transparent() -> void:
 	is_transparent = true
-	$WallMesh.set_surface_override_material(0, transparent_wall_material) 
+	wall_mesh.set_surface_override_material(0, transparent_wall_material) 
 	transparent_tween = create_tween().set_parallel()
 	transparent_tween.tween_property(self, "intensity", 1.0, TWEEN_DURATION)
 
@@ -65,18 +70,19 @@ func _apply_material_normal() -> void:
 	is_transparent = false
 	if transparent_tween:
 		transparent_tween.kill()
-	$WallMesh.set_surface_override_material(0, wall_material)
+	wall_mesh.set_surface_override_material(0, wall_material)
 	intensity = 0
 
 
 func _on_values_changed() -> void:
-	if preview_transparent_material:
-		if transparent_wall_material != null:
-			$WallMesh.set_surface_override_material(0, transparent_wall_material)
+	if wall_mesh != null:
+		if preview_transparent_material:
+			if transparent_wall_material != null:
+				wall_mesh.set_surface_override_material(0, transparent_wall_material)
+			else:
+				wall_mesh.set_surface_override_material(0, null)
 		else:
-			$WallMesh.set_surface_override_material(0, null)
-	else:
-		if wall_material != null:
-			$WallMesh.set_surface_override_material(0, wall_material)
-		else:
-			$WallMesh.set_surface_override_material(0, null)
+			if wall_material != null:
+				wall_mesh.set_surface_override_material(0, wall_material)
+			else:
+				wall_mesh.set_surface_override_material(0, null)
