cda94fa728c47afa01f6dd94cf3592a89ba5cd83
Jacob Meyer <meyer28j@uregina.ca>
2025-03-31 13:40:54 -0600

    Create basic dialogue box with selection text

---
+++ src/ui/dialogue_popup.gd
@@ -0,0 +1,10 @@
+class_name DialoguePopup
+extends MarginContainer
+
+const COLOR_BG: Color = Color(0.13, 0.13, 0.13, 1)
+const COLOR_BG_HOVER: Color = Color(0.2, 0.2, 0.2, 1)
+
+
+func _ready() -> void:
+	pass
+	#visible = false
