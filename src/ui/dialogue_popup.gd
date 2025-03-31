92cf4162a13d0ea54b170ee5346f785b684e6f68
Jacob Meyer <meyer28j@uregina.ca>
2025-03-31 14:12:29 -0600

    Add another response and code to show/hide dialogue

--- src/ui/dialogue_popup.gd
+++ src/ui/dialogue_popup.gd
@@ -1,10 +1,36 @@
 class_name DialoguePopup
 extends MarginContainer
 
-const COLOR_BG: Color = Color(0.13, 0.13, 0.13, 1)
-const COLOR_BG_HOVER: Color = Color(0.2, 0.2, 0.2, 1)
 
+var dialogue_container: VBoxContainer
+var old_man_dialogue: Label
+var responses: Array[Button]
 
 func _ready() -> void:
-	pass
-	#visible = false
+	
+	dialogue_container = $MarginContainer/VBoxContainer/HBoxContainer/Dialogue
+	old_man_dialogue = dialogue_container.get_node("OldManDialogue")
+	for response: Node in dialogue_container.get_node("Responses").get_children():
+		responses.append(response)
+	
+	print("old man says: ", old_man_dialogue.text)
+	print("responses are:")
+	for response: Button in responses:
+		print("\t", response.text)
+	
+	responses[-1].pressed.connect(_on_close_pressed)
+	show_dialogue()
+
+
+func show_dialogue() -> void:
+	visible = true
+	get_tree().paused = true
+
+
+func hide_dialogue() -> void:
+	visible = false
+	get_tree().paused = false
+
+
+func _on_close_pressed() -> void:
+	hide_dialogue()
