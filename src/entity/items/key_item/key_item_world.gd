3a320d6bc90ff7613dd5e091c1ec69be4d85d7aa
Jacob Meyer <meyer28j@uregina.ca>
2025-04-01 17:42:53 -0600

    Add movement functions for key item

--- src/entity/items/key_item/key_item_world.gd
+++ src/entity/items/key_item/key_item_world.gd
@@ -3,6 +3,8 @@ extends ItemWorld
 
 var starting_room: Room
 var movement_path: Array
+var move_target: Vector3
+var move_speed: float = 2
 
 
 func _ready() -> void:
@@ -16,14 +18,55 @@ func _ready() -> void:
 	else:
 		starting_position = get_node("/root/Game").key_item_starting_position
 	
+	SignalBus.key_item_dropped.connect(_on_key_item_dropped)
+	
+	# reactivated when key item is dropped
+	set_process(false)
 	super()
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	# TODO:
 	# slowly moves the key item towards its original position
 	# activated/deactivated when the key item is picked up or dropped
-	pass
+	var direction: Vector3 = move_target - global_position
+	# ensure ghost only moves in xz-plane and does not follow objects up into the air
+	direction.y = 0
+	# force target_pos onto y=1 plane to ensure ghost can "reach" targets
+	# at different heights - i.e. player which is at a lower height
+	move_target = Vector3(move_target.x, 1, move_target.z)
+	var distance_to_target: float = global_position.distance_squared_to(move_target)
+	
+	if distance_to_target < move_speed * delta:
+		var next_target: Room = movement_path.pop_front()
+		if next_target != null:
+			# set target to next room
+			set_target(next_target)
+		else:
+			set_process(false)
+	else:
+		direction = direction.normalized()
+		global_position.lerp(direction, move_speed * delta)
+
+
+func set_target(target_room: Room) -> void:
+	# if target is self, move to center of room
+	var parent_room: Room = get_parent()
+	if target_room == parent_room or target_room == null:
+		move_target = parent_room.global_position
+		return
+	
+	# get current room's available doors
+	var doors: Array[Door] = parent_room.doors.values()
+	# match each door's linked room against the target room
+	for door: Door in doors:
+		if door.linked_room == target_room:
+			# after a match, set door as target
+			move_target = door.global_position
+			return
+	
+	push_error("Key Item unable to find movement target")
+	move_target = self.global_position
 
 
 func reset() -> void:
@@ -46,9 +89,15 @@ func pick_up() -> void:
 	SignalBus.item_picked_up.emit(item_inventory, false)
 	#queue_free()
 	visible = false
+	set_process(false)
 
 
 func _on_body_exited(_body: Node3D) -> void:
 	# override function to display "key item" message again instead of hiding message
 	$Interactable.display_message("KEY ITEM")
 	$Interactable.enabled = false
+
+
+func _on_key_item_dropped() -> void:
+	set_process(true)
+	set_target(movement_path.pop_front())
