c57a81a816d328fdf6ce4f4e687ecd31680a738a
Cameron Oehler <coehler@hotmail.com>
2025-04-01 16:23:55 -0600

    Cleanup shader

--- src/entity/items/item_sprite_backlight.gd
+++ src/entity/items/item_sprite_backlight.gd
@@ -1,6 +1,9 @@
 extends SpriteBacklight
 
+@onready var item_world_shader_material: ShaderMaterial = preload("item_world_shader_material.tres")
+
 func _ready() -> void:
 	super()
 	material = material_override
 	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	material.next_pass = item_world_shader_material
