a99e2a166d72e48786730049355653b5bddb578f
Cameron Oehler <coehler@hotmail.com>
2025-04-03 10:54:14 -0600

    Fix possessable_ranged remaining possessed and never shooting again

--- src/possessable/attack/possessable_ranged.gd
+++ src/possessable/attack/possessable_ranged.gd
@@ -6,17 +6,24 @@ signal projectile_shot
 @export var projectile_scene: PackedScene
 @export var attack_once: bool = false
 
-var has_attacked: bool = false
+
+func _ready() -> void:
+	super()
+	projectile_shot.connect(_on_projectile_shot)
+
 
 func attack(target: Node3D, _attack_windup: float) -> void:
-	if player_in_range and room.player_in_room and (not attack_once or not has_attacked):
-		# disable player detection
-		$AttackRange.collision_mask = 0
-		
+	if player_in_range and room.player_in_room:
 		projectile_shot.emit()
-		has_attacked = true
 		
 		# shoot projectile at target
 		var projectile: Projectile = projectile_scene.instantiate()
 		add_child(projectile)
 		projectile.shoot(target.global_position)
+		
+	depossess(true)
+
+
+func _on_projectile_shot() -> void:
+	if attack_once:
+		room.call_deferred("remove_possessable", self)
