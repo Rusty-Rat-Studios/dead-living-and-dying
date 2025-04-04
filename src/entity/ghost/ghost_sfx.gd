ffbc261b59c91ea34a7028a27a269655b9355b37
Jacob Meyer <meyer28j@uregina.ca>
2025-04-04 00:03:51 -0600

    Refactor multiple audio stream nodes to use base class

--- src/entity/ghost/ghost_sfx.gd
+++ src/entity/ghost/ghost_sfx.gd
@@ -1,56 +1,25 @@
-extends AudioStreamPlayer3D
+extends AudioStreamMultiple
 
 const MAX_DB_QUIET: float = -12 # used in state LIVING
 const MAX_DB_NORMAL: float = -4 # used in state DYING
 
-const WHEEZE_AUDIO_FOLDER_PATH: String = "res://src/sound/ghost/wheeze"
 const DEAD_STING: AudioStream = preload("res://src/sound/ghost/dead_sting.mp3")
 
 
 const DELAY_TIME_MIN: float = 1.5
 const DELAY_TIME_MAX: float = 2.5
 
-var audio_clips: Array[AudioStream]
 var player_dead: bool = false
 
 
 func _ready() -> void:
-	init_audio_clips()
+	audio_folder_path = "res://src/sound/ghost/wheeze"
+	super()
+	
 	# initialize max_db to state living condition
 	max_db = MAX_DB_QUIET
 	# reduce volume if living; increase if dying; disable if dead
 	SignalBus.player_state_changed.connect(_on_player_state_changed)
-
-
-func init_audio_clips() -> void:
-	audio_clips = []
-	var dir_access: DirAccess = DirAccess.open(WHEEZE_AUDIO_FOLDER_PATH)
-	if dir_access == null:
-		printerr("Could not open directory:", WHEEZE_AUDIO_FOLDER_PATH)
-		return
-
-	dir_access.list_dir_begin()
-	var file_name: String = dir_access.get_next()
-	while file_name != "":
-		if not dir_access.current_is_dir() and not file_name.ends_with(".import"):
-			var full_path: String = WHEEZE_AUDIO_FOLDER_PATH.rstrip("/") + "/" + file_name
-			var audio_stream: AudioStream = load(full_path) as AudioStream
-			if audio_stream != null:
-				audio_clips.append(audio_stream)
-			else:
-				printerr("Failed to load audio file:", full_path)
-		file_name = dir_access.get_next()
-
-	dir_access.list_dir_end()
-	print("Loaded", audio_clips.size(), "audio clips from:", WHEEZE_AUDIO_FOLDER_PATH)
-
-
-func enable() -> void:
-	play_random_sound()
-
-
-func disable() -> void:
-	stop()
 
 
 func get_random_audio_clip() -> AudioStream:
@@ -70,16 +39,6 @@ func play_sound() -> void:
 		AudioManager.play_modulated(self)
 	else:
 		play_random_sound()
-
-
-func play_random_sound() -> void:
-	# check if audio already playing - don't interrupt self
-	if playing:
-		return
-	var random_clip: AudioStream = get_random_audio_clip()
-	if random_clip != null:
-		stream = random_clip
-		AudioManager.play_modulated(self)
 
 
 func _on_player_state_changed(state: PlayerStateMachine.States) -> void:
--- src/player/breathing.gd
+++ src/player/breathing.gd
@@ -1,67 +1,29 @@
-extends AudioStreamPlayer3D
-
-const AUDIO_FOLDER_PATH: String = "res://src/sound/sfx/breath"
+extends AudioStreamMultiple
 
 const DELAY_TIME_MIN: float = 1.5
 const DELAY_TIME_MAX: float = 2.5
 
-var audio_clips: Array[AudioStream]
 var delay_timer: Timer = Timer.new()
 
+
 func _ready() -> void:
-	init_audio_clips()
+	audio_folder_path = "res://src/sound/sfx/breath"
+	super()
 	
 	delay_timer.wait_time = RNG.rng.randf_range(DELAY_TIME_MIN, DELAY_TIME_MAX)
 	add_child(delay_timer)
 	delay_timer.timeout.connect(_on_delay_timer_timeout)
 
 
-func init_audio_clips() -> void:
-	audio_clips = []
-	var dir_access: DirAccess = DirAccess.open(AUDIO_FOLDER_PATH)
-	if dir_access == null:
-		printerr("Could not open directory:", AUDIO_FOLDER_PATH)
-		return
-
-	dir_access.list_dir_begin()
-	var file_name: String = dir_access.get_next()
-	while file_name != "":
-		if not dir_access.current_is_dir() and not file_name.ends_with(".import"):
-			var full_path: String = AUDIO_FOLDER_PATH.rstrip("/") + "/" + file_name
-			var audio_stream: AudioStream = load(full_path) as AudioStream
-			if audio_stream != null:
-				audio_clips.append(audio_stream)
-			else:
-				printerr("Failed to load audio file:", full_path)
-		file_name = dir_access.get_next()
-
-	dir_access.list_dir_end()
-	print("Loaded", audio_clips.size(), "audio clips from:", AUDIO_FOLDER_PATH)
-
 
 func enable() -> void:
-	play_random_sound()
+	super()
 	delay_timer.start()
 
 
 func disable() -> void:
+	super()
 	delay_timer.stop()
-	stop()
-
-
-func get_random_audio_clip() -> AudioStream:
-	if audio_clips.is_empty():
-		printerr("No audio clips loaded.")
-		return null
-	var random_index: int = RNG.rng.randi() % audio_clips.size()
-	return audio_clips[random_index]
-
-
-func play_random_sound() -> void:
-	var random_clip: AudioStream = get_random_audio_clip()
-	if random_clip != null:
-		stream = random_clip
-		AudioManager.play_modulated(self)
 
 
 func _on_delay_timer_timeout() -> void:
