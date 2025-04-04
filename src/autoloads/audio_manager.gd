f5915e3fb16518433e628f9f70dbf13ef98a0404
Jacob Meyer <meyer28j@uregina.ca>
2025-04-04 15:02:26 -0600

    Code cleanup

--- src/autoloads/audio_manager.gd
+++ src/autoloads/audio_manager.gd
@@ -2,20 +2,6 @@ extends Node
 
 const PITCH_MAX_STEP: float = 0.05
 
-var db_values: Dictionary[String, float]
-
-func register_sound_group(group_name: String, value: float) -> void:
-	if not db_values.has(group_name):
-		db_values[group_name] = value
-
-
-func set_db_value(group_name: String, value: float) -> void:
-	db_values[group_name] = value
-
-
-func get_db_value(group_name: String) -> float:
-	return db_values[group_name]
-
 
 func play_modulated(audio_player: AudioStreamPlayer3D, from_position: float = 0) -> void:
 	# randomly modulate pitch +/- 10%
