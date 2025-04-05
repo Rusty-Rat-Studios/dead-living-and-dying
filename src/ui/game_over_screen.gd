446f35238df0d6e53144ee59277d60ad15c7df94
Jacob Meyer <meyer28j@uregina.ca>
2025-04-04 18:25:56 -0600

    Add game over screen

---
+++ src/ui/game_over_screen.gd
@@ -0,0 +1,30 @@
+extends Control
+
+const SCENE_SWITCH_DELAY: float = 3
+const FADE_IN_TIME: float = 2
+
+var fade_in: Tween
+
+@onready var scene_switch_timer: Timer = Timer.new()
+@onready var main_menu_scene: PackedScene = load("res://src/ui/menus/main_menu.tscn")
+
+
+func _ready() -> void:
+	modulate.a = 0
+	
+	scene_switch_timer.wait_time = SCENE_SWITCH_DELAY
+	add_child(scene_switch_timer)
+	scene_switch_timer.timeout.connect(_on_scene_switch_timeout)
+	
+	SignalBus.game_over.connect(_on_game_over)
+
+
+func _on_game_over() -> void:
+	fade_in = create_tween()
+	fade_in.tween_property(self, "modulate:a", 1, FADE_IN_TIME)
+	await fade_in.finished
+	scene_switch_timer.start()
+
+
+func _on_scene_switch_timeout() -> void:
+	get_tree().change_scene_to_packed(main_menu_scene)
